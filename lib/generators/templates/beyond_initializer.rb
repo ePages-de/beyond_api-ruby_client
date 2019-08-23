1db798989f78de8eeb80c50f566b9cc51b9891a2
unabris <u.abrisketa@epages.com>
2019-08-23 14:55:26 +0200

    Add Beyond initializer

---
+++ lib/generators/templates/beyond_initializer.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+BeyondAPI.setup do |config|
+  # ==> Client configuration
+  config.client_id = nil
+  config.client_secret = nil
+
+  # ==> API configuration
+  # Configure the open connection timeout in seconds. Default is 2 seconds.
+  # config.open_timeout = 2.seconds
+
+  # Configure the request timeout in seconds. Default is 5 seconds.
+  # config.timeout = 5.seconds
+end
