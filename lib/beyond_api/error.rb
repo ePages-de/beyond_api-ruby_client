9710de1e1fe1f75de43a3998d1a620da4f352a5d
gersanco <g.sanemeterio@epages.com>
2020-06-16 09:52:20 +0200

    Fix rubocop offense

--- lib/beyond_api/error.rb
+++ lib/beyond_api/error.rb
@@ -5,13 +5,13 @@ module BeyondApi
     attr_reader :error_id, :details, :trace_id, :full_message, :status_code, :error, :error_description
 
     def initialize(data, status_code = nil)
-      @error_id               = data['errorId']
-      @details                = data['details']
-      @trace_id               = data['traceId']
-      @error                  = data['error']
-      @error_description      = data['error_description']
-      @full_message           = data
-      @status_code            = status_code
+      @error_id          = data['errorId']
+      @details           = data['details']
+      @trace_id          = data['traceId']
+      @error             = data['error']
+      @error_description = data['error_description']
+      @full_message      = data
+      @status_code       = status_code
 
       super(data['message'] || data['error_description'])
     end
