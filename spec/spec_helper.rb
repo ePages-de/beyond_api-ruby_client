d83089c377c0f26efc92075bf9eea0e79724adc2
gersanco <g.sanemeterio@epages.com>
2020-07-13 15:07:08 +0200

    Add BeyondApi setup for RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "beyond_api"
+require "dotenv/load"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -10,5 +11,15 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+end
+
+unless ENV["CLIENT_ID"].nil? and ENV["CLIENT_SECRET"].nil?
+  BeyondApi.setup do |config|
+    config.client_id = ENV["CLIENT_ID"]
+    config.client_secret = ENV["CLIENT_SECRET"]
+    config.remove_response_links = true
+    config.remove_response_key_underscores = true
+    config.object_struct_responses = true
   end
 end
