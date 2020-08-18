3dca2df0fff2c7bdd96c59115739a1bf61c3a5db
German <g.sanemeterio@epages.com>
2020-08-18 12:48:52 +0200

    v0.12.0.pre
    * features
      * Add locations methods
        * `Locations#all`
        * `Locations#create`
        * `Locations#delete`
        * `Locations#find`
        * `Locations#update`
    
    * enhancements
      * Use `autoload` instead of `require`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require "bundler/setup"
+require "dotenv/load"
 require "beyond_api"
 
 RSpec.configure do |config|
@@ -10,5 +11,15 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+end
+
+unless ENV["CLIENT_ID"].nil? and ENV["CLIENT_SECRET"].nil?
+  BeyondApi.setup do |config|
+    config.client_id = ENV["CLIENT_ID"]
+    config.client_secret = ENV["CLIENT_SECRET"]
+    config.remove_response_links = true
+    config.remove_response_key_underscores = true
+    config.object_struct_responses = true
   end
 end
