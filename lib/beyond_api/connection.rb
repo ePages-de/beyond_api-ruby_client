ec884f40e1de895c6492e52e38c0ff958ed98be1
Unai Abrisketa <u.abrisketa@epages.com>
2021-10-08 14:17:02 +0200

    Add removed line

--- lib/beyond_api/connection.rb
+++ lib/beyond_api/connection.rb
@@ -30,6 +30,7 @@ module BeyondApi
           logger.filter(/(code=)([a-zA-Z0-9]+)/, '\1[FILTERED]')
           logger.filter(/(refresh_token=)([a-zA-Z0-9.\-\_]+)/, '\1[FILTERED]')
         end
+        faraday.headers['Accept'] = 'application/json'
         faraday.adapter(:net_http)
         faraday.basic_auth(BeyondApi.configuration.client_id,
                            BeyondApi.configuration.client_secret)
