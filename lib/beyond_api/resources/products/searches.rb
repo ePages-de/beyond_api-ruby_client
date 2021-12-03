2e272d81a39512d57b05a96eb36023497dab5e7a
Kathia <k.salazar@epages.com>
2021-12-03 12:40:48 +0100

    Release v0.20.0.pre (#44)
    * Add product search
    
    * Fix identation

--- lib/beyond_api/resources/products/searches.rb
+++ lib/beyond_api/resources/products/searches.rb
@@ -5,6 +5,67 @@ require "beyond_api/utils"
 module BeyondApi
   module ProductSearches
     #
+    # A +POST+ request is used to search for products by a search term. Optionally, you can also filter the search results.
+    #
+    #   $ curl 'https://api-shop.beyondshop.cloud/api/products/search' -i -X POST \
+    #       -H 'Content-Type: application/hal+json;charset=UTF-8' \
+    #       -H 'Accept: application/hal+json' \
+    #         -H 'Authorization: Bearer <Access token>'
+    #         -d '{
+    #       "search" : {
+    #         "term" : "Tony Highfinger, Poloshirt, Men",
+    #         "category" : "NAME"
+    #       },
+    #       "filters" : [ {
+    #         "key" : "status",
+    #         "values" : [ "PUBLISHED" ]
+    #       } ],
+    #       "paging" : {
+    #         "page" : 0,
+    #         "pageSize" : 20,
+    #         "sort" : {
+    #           "property" : "name",
+    #           "direction" : "ASC"
+    #         }
+    #       }
+    #   }'
+    #
+    # @beyond_api.scopes +prod:r+
+    #
+    # @param body [String] the request body
+    #
+    # @return [OpenStruct]
+    #
+    # @example
+    #   body = {
+    #     search: {
+    #       term: "Tony Highfinger, Poloshirt, Men",
+    #       category: "NAME"
+    #     },
+    #     filters: [ {
+    #       key: "status",
+    #       values: [ "PUBLISHED" ]
+    #     } ],
+    #     paging: {
+    #       page: 0,
+    #       page_size: 20,
+    #       sort: {
+    #         property: "name",
+    #         direction: "ASC"
+    #       }
+    #     }
+    #   }
+    #   @products = session.product.search(body)
+    #
+    def search(body)
+      response, status = BeyondApi::Request.post(@session,
+                                                 "/products/search",
+                                                 body)
+
+      handle_response(response, status)
+    end
+
+    #
     # A +GET+ request is used to search for a product by SKU.
     #
     #   $ curl 'https://api-shop.beyondshop.cloud/api/products/search/find-by-sku?sku=vino020' -i -X GET \
