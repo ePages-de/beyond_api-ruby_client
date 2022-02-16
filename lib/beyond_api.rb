85bb15406e22634d2c350527a451f97921c9da3f
Unai Abrisketa <u.abrisketa@epages.com>
2022-02-16 10:04:59 +0100

    Move configuration to separated file

--- lib/beyond_api.rb
+++ lib/beyond_api.rb
@@ -8,41 +8,18 @@ loader.ignore("#{__dir__}/ext")
 loader.ignore("#{__dir__}/generators")
 loader.setup
 
-Dir["#{__dir__}/ext*.rb"].each { |file| require file }
+Dir["#{__dir__}/ext/*.rb"].each { |file| require file }
 
 module BeyondApi
   extend BeyondApi::Logger
 
   class << self
-    attr_accessor :configuration
-  end
+    def configuration
+      @configuration ||= ::BeyondApi::Configuration.new
+    end
 
-  def self.setup
-    self.configuration ||= Configuration.new
-
-    yield configuration
-  end
-
-  class Configuration
-    attr_accessor :client_id, :client_secret, :open_timeout, :timeout, :remove_response_links,
-                  :remove_response_key_underscores, :object_struct_responses, :raise_error_requests,
-                  :log_headers, :log_bodies, :log_level, :all_pagination_size
-
-    def initialize
-      @client_id = nil
-      @client_secret = nil
-      @open_timeout = 2
-      @timeout = 5
-      @remove_response_links = false
-      @remove_response_key_underscores = false
-      @object_struct_responses = false
-      @raise_error_requests = false
-
-      @log_level = :info
-      @log_headers = false
-      @log_bodies = false
-
-      @all_pagination_size = 200
+    def setup
+      yield configuration
     end
   end
 end
